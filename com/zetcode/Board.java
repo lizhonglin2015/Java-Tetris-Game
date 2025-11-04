085d481ed9c81522cbef5db2847f75c2986767a6
lizhonglin <lizhonglin@liepin.com>
2025-11-04 19:40:18 +0800

    更改大小

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -16,7 +16,7 @@ public class Board extends JPanel {
 
     private final int BOARD_WIDTH = 10;
     private final int BOARD_HEIGHT = 22;
-    private final int PERIOD_INTERVAL = 300;
+    private final int PERIOD_INTERVAL = 600;
 
     private Timer timer;
     private boolean isFallingFinished = false;
